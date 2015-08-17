6d94194c4bb66e0c6fbea98ade43b57d7010833a
Tjerk Valentijn <tjerk.valentijn@inology.nl>
2015-08-17 13:03:47 +0200

    missing files

---
+++ angular2-client/app/typings/_custom.d.ts
@@ -0,0 +1,10 @@
+/*
+ * Your custom types
+ * Place references (from ./app/typings) to any custom typings you need here
+ */
+/// <reference path="../../tsd_typings/ng2.d.ts" />
+
+/*
+ * tsd generated types
+ */
+/// <reference path="../../tsd_typings/tsd.d.ts" />
